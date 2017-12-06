c09a5f875a75ee7252dae50177b846e94664ddc8
aseijo <alejandro.seijo@gmail.com>
2017-12-06 16:43:40 +0100

    Lectura de sensores

---
+++ sensores.ino
@@ -0,0 +1,37 @@
+#include <DHT.h>
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+#define SENSORTEMP A0
+#define SENSORLUZ A1
+
+float dhtHum;
+float dhtTemp;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+
+  dhtTemp = dht.readTemperature();
+  dhtHum = dht.readHumidity();
+  
+  int valorTemp = analogRead(SENSORTEMP);
+  int valorLuz = analogRead(SENSORLUZ);
+  float grados = ((valorTemp / 1023.0) * 5000) / 10;
+  
+  Serial.print("DHTTemp:");
+  Serial.println(dhtTemp);  
+  Serial.print("DHTHum:");
+  Serial.println(dhtHum);
+  Serial.print("Luz:");
+  Serial.println(valorLuz);
+  
+  delay(2000);
+  
+}
